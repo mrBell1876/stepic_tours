e6eb2654119298039db709967b0ad1b4394846a6
Max Zlobin <m@mzlobin.ru>
2020-11-19 19:40:02 +0300

    add red line in import(pep8)

--- tours/urls.py
+++ tours/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+
 from tours.views import MainView, DepartureView, TourView
+
+
 urlpatterns = [
     path('', MainView.as_view()),
     path('departure/<str:departure>', DepartureView.as_view()),
